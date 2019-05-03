14ccc7bdf538c3228671b9d85cf4519b4e6cf094
Jille <jille.van.der.weerd@beeproger.com>
2019-05-03 22:49:35 +0200

    removes console log

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,7 +25,6 @@ export class HomePage {
   @HostListener('window:resize')
   onResize() {
     this.pageWidth = this.platform.width();
-    console.log(this.pageWidth);
   }
 
   get containerClass(): string {
